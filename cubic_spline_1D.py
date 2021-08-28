b92a82095b00b1c5a71176b41273b5a765259379
kazimgorgulu <kgorgulu18@ku.edu.tr>
2021-08-28 16:40:33 +0300

    enh: getData fcn added

--- cubic_spline_1D.py
+++ cubic_spline_1D.py
@@ -55,38 +55,33 @@ def calc_coeff(x, y):
     return linalg.solve(A,b)
 
 
-def getData(p, min_x, max_x, len_x,c):
+def getData(p, x, c):
     p = np.array(p)
-    d = (max_x-min_x)/(len_x-1)
-    print(p)
-    # print(np.array((p-min_x)/d).astype(int))
-    ind = np.array((p-min_x)/d).astype(int)
-    print(ind)
-    f = np.zeros(ind.shape[0])
-    print(f)
-    for k in range(ind.shape[0]):
-        i = ind * 4
-        print(c[i] * p[k] ** 3 + c[i + 1] * p[k] ** 2 + c[i + 2] * p[k] + c[i + 3])
-        print(c[i])
-        f[k] = c[i] * p[k] ** 3 + c[i + 1] * p[k] ** 2 + c[i + 2] * p[k] + c[i + 3]
+    x = np.array(x)
+    f = np.zeros(p.shape[0])
+
+    for k in range(p.shape[0]):
+        ind = np.abs(x - p[k]).argmin()
+
+        if (x[ind] > p[k] or ind == x.shape[0]-1) and not ind == 0:
+            ind -= 1
+        n = ind * 4
+        f[k] = c[n] * p[k] ** 3 + c[n + 1] * p[k] ** 2 + c[n + 2] * p[k] + c[n + 3]
 
     return f
 
 
 
-x = np.array([0., 1, 2])
-y = np.array([1., 1.5, 1])
+x = np.array([0., 1, 2, 4, 5, 6])
+y = np.array([1., 2, 1, 0, -1, 0])
 
 c = calc_coeff(x, y)
 print(c)
-# ind = getData([0,1],min(x), max(x), len(x),c)
 
-# print(ind)
-p = np.linspace(0,4,100)
+p = np.linspace(-2,8,100)
 f = np.zeros(p.size)
 
-ind = 0
-
+print('p: ', p)
 
 for k in range(p.shape[0]):
     if p[k] >= 1:
@@ -96,11 +91,19 @@ for k in range(p.shape[0]):
     i = ind * 4
     f[k] = c[i] * p[k] ** 3 + c[i+1] * p[k] ** 2 + c[i+2] * p[k] + c[i+3]
 
+print(f)
+
+f2 = getData(p,x,c)
+
+print(f2)
+
+
 
 import matplotlib.pyplot as plt
 
 plt.figure()
-plt.plot(p,f)
+plt.plot(p,f2)
+
 plt.plot(x,y,'o')
 plt.show()
 
